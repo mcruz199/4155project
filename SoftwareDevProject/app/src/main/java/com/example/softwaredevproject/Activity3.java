0f70535042bd66bb44d333183511ff011b47e9ae
mcruz199 <mcruz19@uncc.edu>
2022-05-07 18:03:54 -0400

    added location service functionality
    + emergency phone navigation

--- SoftwareDevProject/app/src/main/java/com/example/softwaredevproject/Activity3.java
+++ SoftwareDevProject/app/src/main/java/com/example/softwaredevproject/Activity3.java
@@ -1,5 +1,21 @@
 package com.example.softwaredevproject;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
